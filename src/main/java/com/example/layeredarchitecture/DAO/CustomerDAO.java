f69d79685ee017547fa7df6399816192c33c22d9
dvish2003 <Vishanchathuranga2003@gmail.com>
2024-06-15 19:37:49 +0530

    Initial commit

---
+++ src/main/java/com/example/layeredarchitecture/DAO/CustomerDAO.java
@@ -0,0 +1,22 @@
+package com.example.layeredarchitecture.DAO;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public interface CustomerDAO {
+     ArrayList<CustomerDTO> getAllCustomer() throws SQLException, ClassNotFoundException;
+
+     void saveCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException ;
+
+     void updateCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException ;
+
+     void delete (String id) throws SQLException, ClassNotFoundException ;
+
+
+     String generateNewId () throws SQLException, ClassNotFoundException ;
+
+     boolean existCustomer(String id) throws SQLException, ClassNotFoundException ;
+}
